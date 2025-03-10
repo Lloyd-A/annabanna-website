5d267cbca622392149a35591a1873f477df8a2c4
Lloyd Allen <lloyd@Lloyds-Laptop.local>
2025-03-09 21:00:17 -0500

    create popular tours section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,15 @@
 import LOGO from "./TempLogo.png";
 import LOGO2 from "./TempLogo2.png";
+import HERO from "./WhiteRiver.png";
+import RIU from "./riu.jpeg";
+import JAM from "./jamaica.jpeg";
+import HOPE from "./jamaica-kingston-hope-botanical-gardens-PFRKKR.jpg";
+import JAM_INN from "./Jamaica Inn_Teddy's Grill.jpeg";
+import BOB from "./Bob-Marley-Museum-Royalty.jpg";
 
-export { LOGO, LOGO2 };
+export { LOGO, LOGO2, HERO };
+export { default as BOB } from "./Bob-Marley-Museum-Royalty.jpg";
+export { default as JAM_INN } from "./Jamaica Inn_Teddy's Grill.jpeg";
+export { default as HOPE } from "./jamaica-kingston-hope-botanical-gardens-PFRKKR.jpg";
+export { default as JAM } from "./jamaica.jpeg";
+export { default as RIU } from "./riu.jpeg";
