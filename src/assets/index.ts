ec0e5c0ee1d5306f22de36920e2c5f054b7628fe
Lloyd Allen <lloyd@Lloyds-Laptop.local>
2025-03-13 11:26:52 -0500

    add driver certifications component

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,6 @@
 import LOGO from "./TempLogo.png";
 import LOGO2 from "./TempLogo2.png";
 import HERO from "./WhiteRiver.png";
-import RIU from "./riu.jpeg";
-import JAM from "./jamaica.jpeg";
-import HOPE from "./jamaica-kingston-hope-botanical-gardens-PFRKKR.jpg";
-import JAM_INN from "./Jamaica Inn_Teddy's Grill.jpeg";
-import BOB from "./Bob-Marley-Museum-Royalty.jpg";
 
 export { LOGO, LOGO2, HERO };
 export { default as BOB } from "./Bob-Marley-Museum-Royalty.jpg";
@@ -13,3 +8,7 @@ export { default as JAM_INN } from "./Jamaica Inn_Teddy's Grill.jpeg";
 export { default as HOPE } from "./jamaica-kingston-hope-botanical-gardens-PFRKKR.jpg";
 export { default as JAM } from "./jamaica.jpeg";
 export { default as RIU } from "./riu.jpeg";
+export { default as JTB } from "./jamaica_tourist_board.png";
+export { default as TPDCO } from "./TPDCo_LOGO.png";
+export { default as TEAM_JA } from "./team_jamaica_logo.png";
+export { default as OCHO_RIOS } from "./ocho_rios.jpg";
