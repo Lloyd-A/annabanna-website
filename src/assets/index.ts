612fb32226f4f6ccc253847fd659e1a15f321104
Lloyd Allen <lloyd@Lloyds-Laptop.local>
2025-03-15 18:12:07 -0500

    add waterfall image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -13,3 +13,4 @@ export { default as TPDCO } from "./TPDCo_LOGO.png";
 export { default as TEAM_JA } from "./team_jamaica_logo.png";
 export { default as OCHO_RIOS } from "./ocho_rios.jpg";
 export { default as CERT } from "./certificate_stub.png";
+export { default as FALLS } from "./Waterfall.png";
